c743ea6ade800f5166eb0c241580cbafa6de9584
Abdullah Khan <mabdullahnk@gmail.com>
2023-12-14 17:32:52 -0500

    feat: new index page added

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ app.use(express.static(__dirname + '/public'));
 // const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1ZWZsd2FzZ3Bucmdjd3NvcW9wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI1MjM3MTAsImV4cCI6MjAxODA5OTcxMH0.LkJmwOK29RFHNWG-_xhBMyFa0igYXw5yHexUAg1XwgA'
 // const supabase = supabaseClient.createClient(supabaseURL, supabaseKey);
 
-app.get('/index.html', (req, res) => {
-    res.sendFile('public/index.html', {root: __dirname})
+app.get('/home.html', (req, res) => {
+    res.sendFile('public/home.html', {root: __dirname})
 })
 
 app.listen(port, () => {
